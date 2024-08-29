3382251bf61c3acd0b357a330cd6680216246630
Nicu G <guliman.nicu@gmail.com>
2024-08-29 17:48:24 +0300

    wip

--- src/components/DonateButton.js
+++ src/components/DonateButton.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHandHoldingDollar } from '@fortawesome/free-solid-svg-icons';
+import { faHandHoldingDollar, faQrcode, faWallet } from '@fortawesome/free-solid-svg-icons';
 import { useTranslation } from 'next-i18next';
 import DApp from "@/components/walletconnect/DApp";
 import QRCodeComponent from "@/components/QRCodeComponent";
@@ -47,43 +47,49 @@ const DonateButton = () => {
 
       {showModal && (
         <div className="fixed z-10 inset-0 bg-black bg-opacity-50 backdrop-filter backdrop-blur-lg overflow-y-auto flex items-center justify-center">
-          <div className="bg-black rounded-lg max-w sm:max-w-md w-full bg-opacity-50 sm:bg-opacity-100 h-full sm:h-auto sm:max-h-screen flex flex-col text-center shadow-md">
+          <div className="bg-black sm:rounded-lg max-w sm:max-w-md w-full bg-opacity-50 sm:bg-opacity-100 h-full sm:h-auto sm:max-h-screen flex flex-col text-center shadow-md">
 
             {/* Modal Header */}
-            <div className="relative text-white py-4 px-6 flex justify-between items-center">
-              <p className="text-xl font-bold">{t('donateComponent.modalTitle')}</p>
-              <button
-                className="absolute top-4 right-4 text-white hover:text-gray-400"
-                onClick={() => setShowModal(false)}
-              >
-                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                     xmlns="http://www.w3.org/2000/svg">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
-                </svg>
-              </button>
+            <div className="flex flex-col bg-black sm:bg-transparent">
+              {/* First Row: Title and Close Button */}
+              <div className="relative text-white py-4 px-6 flex justify-between items-center">
+                <p className="text-xl font-bold">{t('donateComponent.modalTitle')}</p>
+                <button
+                  className="absolute top-4 right-4 text-white hover:text-gray-400"
+                  onClick={() => setShowModal(false)}
+                >
+                  <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+                  </svg>
+                </button>
+              </div>
+
+              {/* Second Row: Tabs */}
+              <div className="flex">
+                <button onClick={() => setActiveTab('wallet')} className={`flex-1 flex items-center justify-center py-2 font-bold text-center rounded-t border-b-2 ${activeTab === 'wallet' ? 'text-white border-white' : 'border-b-1 text-gray-400 border-gray-400'}`}>
+                  <div className="w-4 h-4 flex items-center justify-center mr-2">
+                    <FontAwesomeIcon icon={faWallet}/>
+                  </div>
+                  <span className="text-md uppercase">Wallet Connect</span>
+
+                </button>
+                <button onClick={() => setActiveTab('qr')} className={`flex-1 flex items-center justify-center py-2 font-bold text-center rounded-t border-b-2 ${activeTab === 'qr' ? 'text-white border-white' : 'border-b-1 text-gray-400 border-gray-400'}`}>
+                  <div className="w-4 h-4 flex items-center justify-center mr-2">
+                    <FontAwesomeIcon icon={faQrcode}/>
+                  </div>
+                  <span className="text-md uppercase">Scan QR Code</span>
+
+                </button>
+              </div>
+
+
             </div>
 
             {/* Modal Content (Scrollable if needed) */}
             <div className="flex-1 overflow-y-auto p-4">
-              {/* Tabs */}
-              <div className="flex justify-center mb-4">
-                <button
-                  onClick={() => setActiveTab('wallet')}
-                  className={`px-4 py-2 font-bold rounded-t ${activeTab === 'wallet' ? 'bg-white text-black' : 'bg-gray-300 text-gray-600'}`}
-                >
-                  Wallet Connect
-                </button>
-                <button
-                  onClick={() => setActiveTab('qr')}
-                  className={`px-4 py-2 font-bold rounded-t ${activeTab === 'qr' ? 'bg-white text-black' : 'bg-gray-300 text-gray-600'}`}
-                >
-                  Scan QR Code
-                </button>
-              </div>
-
               {/* Tab Content */}
-              {activeTab === 'wallet' && <DApp />}
-              {activeTab === 'qr' && <QRCodeComponent address={ethAddress} amount={ethAmount.toString()} />}
+              {activeTab === 'wallet' && <DApp/>}
+              {activeTab === 'qr' && <QRCodeComponent address={ethAddress} amount={ethAmount.toString()}/>}
             </div>
 
             {/* Modal Footer */}
@@ -98,6 +104,7 @@ const DonateButton = () => {
           </div>
         </div>
       )}
+
     </>
   );
 };
