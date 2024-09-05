7691241fe57285ed57868fd73e90f86b95050fa5
Nicu G <guliman.nicu@gmail.com>
2024-09-05 15:57:18 +0300

    some clean-up

--- backend/webSocket.js
+++ backend/webSocket.js
@@ -8,7 +8,7 @@ const startWebSocketServer = (server) => {
   wss = new WebSocket.Server({ noServer: true });
 
   wss.on('connection', (ws) => {
-    console.log('New WebSocket connection established');
+    console.log('WEBSOCKET: New connection established');
 
     // Send recent messages to the newly connected client
     recentMessages.forEach((message) => {
@@ -16,32 +16,32 @@ const startWebSocketServer = (server) => {
     });
 
     ws.on('message', (message) => {
-      console.log('Received message from client:', message);
+      console.log('WEBSOCKET: Received message from client:', message);
     });
 
     ws.on('close', () => {
-      console.log('WebSocket connection closed');
+      console.log('WEBSOCKET: Connection closed');
     });
 
     ws.on('error', (error) => {
-      console.error('WebSocket error:', error);
+      console.error('WEBSOCKET: Error:', error);
     });
   });
 
   server.on('upgrade', (request, socket, head) => {
-    console.log('Handling WebSocket upgrade request');
+    console.log('WEBSOCKET: Handling upgrade request');
 
     try {
       wss.handleUpgrade(request, socket, head, (ws) => {
-        console.log('WebSocket upgrade handled, emitting connection event');
+        console.log('WEBSOCKET: Upgrade handled, emitting connection event');
         wss.emit('connection', ws, request);
       });
     } catch (error) {
-      console.error('Error handling WebSocket upgrade:', error);
+      console.error('WEBSOCKET: Error handling upgrade:', error);
     }
   });
 
-  console.log('WebSocket server started');
+  console.log('WEBSOCKET: Server started');
 
   // Set up periodic buffer clearing every 5 minutes (300000 ms)
   setInterval(clearBuffer, 300000);
@@ -50,24 +50,24 @@ const startWebSocketServer = (server) => {
 // Broadcast a message to all connected clients and store it in the buffer
 const broadcastMessage = (message) => {
   if (wss) {
-    console.log('Attempting to broadcast message:', message);
+    console.log('WEBSOCKET: Attempting to broadcast message:', message);
     recentMessages.push(message); // Store message in buffer
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
-        console.log('Sending message to client:', message);
+        console.log('WEBSOCKET: Sending message to client:', message);
         client.send(JSON.stringify(message));
       } else {
-        console.log('Client is not open. ReadyState:', client.readyState);
+        console.log('WEBSOCKET: Client is not open. ReadyState:', client.readyState);
       }
     });
   } else {
-    console.error('WebSocket Server (wss) is not defined.');
+    console.error('WEBSOCKET: Server (wss) is not defined.');
   }
 };
 
 // Function to clear the buffer
 const clearBuffer = () => {
-  console.log('Clearing recent messages buffer');
+  console.log('WEBSOCKET: Clearing recent messages buffer');
   recentMessages = []; // Clear the buffer
 };
 
