e9b63699e144b3a56f04014d670f786c36e8b35c
Nicu G <guliman.nicu@gmail.com>
2024-10-26 20:31:39 +0300

    try this

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backdropBlur: {
+        md: '10px', // Define the blur amount you want
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
