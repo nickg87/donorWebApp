bb26dc95cfa3b88f7ff62182e9c271ee415df0b8
Nicu G <guliman.nicu@gmail.com>
2024-11-05 17:39:06 +0200

    fix firstTextTile

--- src/components/IndexTiles.js
+++ src/components/IndexTiles.js
@@ -37,7 +37,7 @@ const IndexTiles = () => {
               </div>
               <div dangerouslySetInnerHTML={{
                 __html: t('firstTextTile', {
-                  var1: poolEntryAmountInDollars
+                  var1: poolPrizeAmountInDollars
                 }),
               }}/>
             </div>
