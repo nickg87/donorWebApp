751bb06c13cf02ce3531061536a9e53366f70afa
Nicu G <guliman.nicu@gmail.com>
2024-11-06 13:04:59 +0200

    fix modal transparency

--- src/components/UI/ModalContainer.js
+++ src/components/UI/ModalContainer.js
@@ -6,8 +6,8 @@ const ModalContainer = ({ children, show, theme }) => {
   return (
     <>
       {show && (
-        <div className={`fixed z-[10] inset-0 ${theme === 'dark' ?  'bg-black' : 'bg-white'} ${classes.modalWrapper} bg-opacity-50 backdrop-filter backdrop-blur-lg overflow-y-auto flex items-center justify-center`}>
-          <div className={`p-8 rounded-[30px]  max-w-full sm:max-w-[454px] mx-4 ${classes.modalContentWrapper} ${theme} border backdrop-blur-md ${theme === 'dark' ? 'border-darkBorder bg-[#030A31] bg-opacity-80 shadow-darkTheme' : 'border-lightBorder bg-white/54 shadow-lightTheme'} `}>
+        <div className={`fixed z-[10] inset-0 ${classes.modalWrapper} bg-opacity-50 backdrop-filter backdrop-blur-lg overflow-y-auto flex items-center justify-center`}>
+          <div className={`p-8 rounded-[30px]  max-w-full sm:max-w-[454px] mx-4 ${classes.modalContentWrapper} ${theme} border backdrop-blur-md ${theme === 'dark' ? 'border-darkBorder bg-[#030A31]/80 shadow-darkTheme' : 'border-lightBorder bg-white/80 shadow-lightTheme'} `}>
             <div className="flex flex-col">
               {children}
             </div>
