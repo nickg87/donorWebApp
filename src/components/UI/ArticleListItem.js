93ba1b7d197a0cdc5613cbe6c57cd163d4ead251
Nicu G <guliman.nicu@gmail.com>
2024-11-13 09:57:53 +0200

    list blog articles styled

---
+++ src/components/UI/ArticleListItem.js
@@ -0,0 +1,57 @@
+import Link from "next/link";
+import React from "react";
+import classes from './ArticleListItem.module.scss';
+import {useAppContext} from "@/contexts/AppContext";
+import {useTranslation} from "next-i18next";
+import ButtonWrapper from "@/components/UI/ButtonWrapper";
+
+const ArticleListItem = ({ article }) => {
+
+  const { t } = useTranslation();
+  const { i18n } = useTranslation();
+  const { globalState } = useAppContext();
+
+  return (
+    <div
+      key={article.slug}
+      className={`${classes.articleWrapper} p-8 rounded-lg shadow-lg transition-shadow hover:shadow-lg backdrop-blur`}
+      style={{
+        backgroundImage: article.files?.[0]?.path ? `url(${article.files[0].path})` : 'none',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      {/* Overlay for increased opacity on the image */}
+      <div
+        className={`${classes.articleContainer} ${classes[globalState?.theme]} absolute inset-0 rounded-lg`}
+        style={{
+          opacity: 0.7, // Adjust this value for the desired transparency level
+          backgroundBlendMode: 'multiply',
+        }}
+      ></div>
+
+      {/* Article content */}
+      <div className="relative z-10">
+        <Link href={`/blog/${article.slug}`}>
+          <h2 className="text-2xl font-semibold hover:text-blue-300 transition-colors">
+            {article.title[i18n.language]}
+          </h2>
+        </Link>
+        <p className="mt-2">{article.short[i18n.language]}</p>
+        <Link href={`/blog/${article.slug}`} className="text-sm text-orange-400 hover:underline mt-4 inline-block">
+          <ButtonWrapper theme={'dark'} extra={'h-[50px] w-[136px]'}>
+            {t('blog.listReadMore')}
+          </ButtonWrapper>
+        </Link>
+
+      </div>
+    </div>
+
+
+  );
+};
+
+export default ArticleListItem;
+
+
+
--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,15 +1,14 @@
 // src/pages/blog/index.js
 import axios from 'axios';
-import Link from 'next/link';
-import {serverSideTranslations} from "next-i18next/serverSideTranslations";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Head from "next/head";
 import React from "react";
-import {useTranslation} from "next-i18next";
+import { useTranslation } from "next-i18next";
+import { useAppContext } from "@/contexts/AppContext";
+import ArticleListItem from "@/components/UI/ArticleListItem";
 
 export default function Blog({ articles }) {
-  console.log(articles);
   const { t } = useTranslation();
-  const { i18n } = useTranslation();
 
   return (
     <>
@@ -17,34 +16,25 @@ export default function Blog({ articles }) {
         <title>{t('blog.listTitleText')}</title>
       </Head>
       <div className="container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold text-center mb-6">{t('blog.listTitleText')}</h1>
-        <ul className="space-y-6">
+        <h1 className="text-4xl font-bold text-left mb-10">{t('blog.listTitleText')}</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
           {articles.map((article) => (
-            <li key={article.slug} className="p-6 border border-gray-300 rounded-lg shadow-lg hover:shadow-xl transition-shadow">
-              <Link className="text-2xl font-semibold text-blue-600 hover:text-blue-800 transition-colors" href={`/blog/${article.slug}`}>
-                  {article.title[i18n.language]}
-              </Link>
-              <p className="text-gray-700 mt-2">{article.short[i18n.language]}</p>
-              <Link className="text-sm text-blue-500 hover:underline mt-4 inline-block" href={`/blog/${article.slug}`}>
-               Read more
-              </Link>
-              {article.files?.[0].path && <img src={article.files?.[0].path} alt={'Image for ' + article.description[i18n.language]}/>}
-            </li>
+              <ArticleListItem article={article}/>
           ))}
-        </ul>
+        </div>
       </div>
     </>
   );
 }
 
 // Fetch the list of articles to display on the /blog page
-export async function getServerSideProps({ locale }) {
+export async function getServerSideProps({locale}) {
   try {
     const apiUrl = process.env.NEXT_PUBLIC_BACKEND_API_URL;
     const response = await axios.get(`${apiUrl}articles`);
 
     if (!response.data || response.data.length === 0) {
-      return { notFound: true }; // Return 404 if no articles are found
+      return {notFound: true}; // Return 404 if no articles are found
     }
 
     const articles = response.data;
@@ -57,6 +47,6 @@ export async function getServerSideProps({ locale }) {
     };
   } catch (error) {
     console.error('Error fetching articles:', error);
-    return { notFound: true }; // Return 404 if there's an error
+    return {notFound: true}; // Return 404 if there's an error
   }
 }
